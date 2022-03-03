096539881a076fde0bdc59a589c6f29d6906c22a
Milana Maksina <m_maksinaa@mail.ru>
2022-03-03 22:37:04 +0300

    HW1 add methods

---
+++ HW Methods/Program.cs
@@ -0,0 +1,10 @@
+﻿using HW_Methods;
+double a = Convert.ToInt32(Console.ReadLine());
+double b = Convert.ToInt32(Console.ReadLine());
+double c = Convert.ToInt32(Console.ReadLine());
+double d = Convert.ToInt32(Console.ReadLine());
+
+
+string result = Variables.GetStraightLineEquation(a, b, c, d);
+Console.WriteLine(result);
+
---
+++ HW Methods/Variables.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Methods
+{
+    static class Variables
+    {
+        public static int GetResultOftheExpression(int a, int b)
+        {
+            int result = (5 * a + b * b) / (b - a);
+            return result;
+        }
+
+        public static int FindXLinearEquation(int a, int b, int c)
+        {
+            int x = (c - b) / a;
+            return x;
+        }
+        public static string GetStraightLineEquation(double x1, double y1, double x2, double y2)
+        {
+            double pointA = (y1 - y2) / (x1 - x2);
+            double pointB = y2 - pointA * x2;
+            string point1 = Convert.ToString(pointA);
+            string point2 = Convert.ToString(pointB);
+            string result = ($" { point2 } {point1}");
+            return result;
+
+        }
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
